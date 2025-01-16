c3f67d5f0ac4bfb2cdc70f49708b73c402fc58b1
Cosmonymous <cosmonymous.mli@gmail.com>
2025-01-15 20:00:48 -0500

    [2.10] hypersnake added

---
+++ assets/games/hypersnake/html5game/yph_Playtime.js
@@ -0,0 +1,15 @@
+var PT_PLAYTIME_USE       = true;
+
+function PT_sendScore(currentScore)
+{
+	if (PT_PLAYTIME_USE)
+	{
+
+	if (window.hasOwnProperty('Playtime')) {
+	    window['Playtime'].sendPostMessage(top, currentScore);
+	}
+
+	console.log("Playtime : score sent " + currentScore.toString());
+
+	}
+}
