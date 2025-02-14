ec93043e77ce80326cfd45a03646bdefa2907957
Cosmonymous <cosmonymous.mli@gmail.com>
2025-02-14 06:50:06 -0500

    [v2.18] - time warriors test, balls incremental and fixed some issues

--- assets/games/escaperoad/src/config/site.ts
+++
@@ -1,7 +0,0 @@
-export const siteConfig = {
-    // REQUIRED
-    appName: "Escape Road",
-    domainName: "escaperoad.world",
-    appDescription: "Play Escape Road",
-    github: "https://github.com/gangbo/escaperoad.world.site",
-}
